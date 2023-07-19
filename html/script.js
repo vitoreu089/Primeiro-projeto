348cedd737575e4677d4831eb9a0da3bf8e1ede5
Vitor <vitoremanuelsc089@gmail.com>
2023-07-19 18:42:13 -0300

    cabo o codi initial commit

---
+++ html/script.js
@@ -0,0 +1,25 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle('light')
+  
+  /*da pra resumir tudo isso, no que esta la embaixo:
+  const html = document.documentElement
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+*/
+//pegar a imagem:
+const img = document.querySelector("#profile img")
+
+//substituir a imagem 
+if (html.classList.contains("light")) {
+  //se tiver light mode, adicionar a imagem light mode
+  img.setAttribute("src", "./assets/assets/avatar-light.png")
+}else{
+  //se não tiver light mode, manter a imagem
+  img.setAttribute("src", "./assets/assets/avatar.png")
+}
+  
+}
